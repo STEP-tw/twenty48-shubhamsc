716484f981b0c465df68f3438ab6d57ca9c94985
Subham Singh <s.chauhan5505@gmail.com>
2019-02-22 11:39:42 +0530

    Implemented move left, right, up and down methods

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,22 +1,20 @@
 (ns twenty48.core
   (:gen-class))
 
-(defn move-grid-right
-  "Moves an entire grid to the right"
-  [grid]
-  grid)
+(def not-zero? (comp not zero?))
+(def list-without-zero (partial filter not-zero?))
+(def transpose (partial apply mapv vector))
+(def add-zero-to-list (comp (partial take 4) (partial flatten) (partial conj (repeat 4 0))))
+(def move (comp
+           (partial add-zero-to-list)
+           (partial map (partial apply +))
+           (partial partition-by identity)
+           (partial list-without-zero)))
 
-(defn move-grid-left
-  "Moves an entire grid to the left"
-  [grid]
-  grid)
+(def move-grid-left "Moves an entire grid to the left" (partial map move))
 
-(defn move-grid-down
-  "Moves an entire grid down"
-  [grid]
-  grid)
+(def move-grid-right "Moves an entire grid to the right" (comp (partial map reverse) (partial move-grid-left) (partial map reverse)))
 
-(defn move-grid-up
-  "Moves an entire grid up"
-  [grid]
-  grid)
+(def move-grid-down "Moves an entire grid down" (comp (partial transpose) (partial move-grid-right) (partial transpose)))
+
+(def move-grid-up "Moves an entire grid up" (comp (partial transpose) (partial move-grid-left) (partial transpose)))
